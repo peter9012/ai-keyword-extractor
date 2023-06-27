1b9081281c0c28997f17f7ebd9759aa74fd19822
peter9012 <rambo_lu@hotmail.com>
2023-06-26 22:13:28 -0700

    Textinput

--- src/components/TextInput.jsx
+++ src/components/TextInput.jsx
@@ -1,8 +1,39 @@
+import { useState } from "react"
+import { Textarea, Button, useToast} from "@chakra-ui/react"
 
 const TextInput = () => {
+  const [text, setText] = useState('');
+
+  const toast = useToast();
+  
+  const submitText = () => {
+    console.log(text);
+  };
+
   return (
-    <div>TextInput</div>
-  )
-}
+    <>
+    <Textarea
+      bg='blue.400'
+      padding={4}
+      marginTop={6}
+      height={200}
+      color='white'
+      value={text}
+      onChange={(e) => setText(e.target.value)}
+    />
+
+    <Button
+      bg='blue.500'
+      color='white'
+      marginTop={4}
+      width='100%'
+      _hover={{ bg: 'blue.700' }}
+      onClick={submitText}
+    >
+      Extract Keywords
+    </Button>
+  </>
+  );
+};
 
 export default TextInput
