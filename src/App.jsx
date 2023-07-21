2532b8af9ccec503d23f6a3931380e975845acd2
peter9012 <rambo_lu@hotmail.com>
2023-07-20 23:23:17 -0700

    API request

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,33 @@ import TextInput from './components/TextInput';
 
 const App = () => {
   const [keywords, setKeywords] = useState('');
-  
-  const extractKeywords = (text) => {
-    console.log(text);
-  }
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const extractKeywords = async (text) => {
+    setLoading(true);
+    setIsOpen(true);
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt:
+          'Extract keywords from this text. Make the first letter of every word uppercase and separate with commas:\n\n' +
+          text +
+          '',
+        temperature: 0.5,
+        max_tokens: 60,
+        top_p: 1.0,
+        frequency_penalty: 0.8,
+        presence_penalty: 0.0,
+      }),
+    }
+  };
 
   return (
     <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
