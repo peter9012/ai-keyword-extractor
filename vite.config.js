cf585e823c831f0821800b556d775a40a7095983
peter9012 <rambo_lu@hotmail.com>
2023-07-31 18:01:58 -0700

    minor fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,8 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/ai-keyword-extractor/",
   plugins: [react()],
-})
+  server: {
+    port: 3000,
+  },
+});
